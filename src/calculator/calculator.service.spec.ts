8e4ea278ab0d4ee167cd06dc26ce9a615cc6c330
Laura <laura@upschool.fr>
2024-06-25 11:16:49 +0200

    create test - failed

--- src/calculator/calculator.service.spec.ts
+++ src/calculator/calculator.service.spec.ts
@@ -15,4 +15,8 @@ describe('CalculatorService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  it('should return 0', () => {
+    expect(service.sum('')).toBe(0);
+  });
 });
