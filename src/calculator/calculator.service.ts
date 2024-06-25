0901383c58d16c42bfd4569761a0c33eb966c405
Laura <laura@upschool.fr>
2024-06-25 11:45:36 +0200

    create test5 - passed

--- src/calculator/calculator.service.ts
+++ src/calculator/calculator.service.ts
@@ -5,6 +5,7 @@ export class CalculatorService {
   sum(string: string): number {
     console.log('string', string);
     if (string === '') return 0;
+    if (isNaN(Number(string))) throw new Error('The string is not a number');
     return Number(string);
   }
 }
