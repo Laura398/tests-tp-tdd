7c50cf0b2d6d00cfff1adf0862aa6b6534a98391
Benoit Belloc <belloc.benoitjean@gmail.com>
2024-06-25 11:27:54 +0200

    create test2 - passed

--- src/calculator/calculator.service.ts
+++ src/calculator/calculator.service.ts
@@ -5,8 +5,11 @@ export class CalculatorService {
   sum(number1: string, number2?: string): number {
     console.log('number1', number1);
     console.log('number2', number2);
-    if (!number1 || (number1 === '' && !number2)) {
-      return 0;
+    if (!number2) {
+      if (!number1 || number1 === '') {
+        return 0;
+      }
+      return Number(number1);
     }
     return;
   }
