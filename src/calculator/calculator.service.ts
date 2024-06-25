264ae47d0be3c4487b73f028d9af041899a8b396
Laura <laura@upschool.fr>
2024-06-25 13:39:44 +0200

    add tests15 - passed

--- src/calculator/calculator.service.ts
+++ src/calculator/calculator.service.ts
@@ -6,9 +6,14 @@ export class CalculatorService {
   add(string: string): number {
     console.log('string', string);
     if (string === '') return 0;
-    if (string.includes(',')) {
-      const numbers = string.split(/,|\n/);
-      return sumNumbers(numbers.map(Number));
+    if (string.includes(',') || string.includes('\n')) {
+      const numbersAsStrings = string.split(/,|\n/);
+      const numbers = numbersAsStrings.map((numberAsString) => {
+        if (numberAsString === '')
+          throw new Error('The string is not well formatted');
+        return Number(numberAsString);
+      });
+      return sumNumbers(numbers);
     }
     if (isNaN(Number(string))) throw new Error('The string is not a number');
     return Number(string);
--- src/calculator/helpers/sumNumbers.ts
+++ src/calculator/helpers/sumNumbers.ts
@@ -2,5 +2,5 @@ export function sumNumbers(numbers: number[]) {
   numbers.forEach((number) => {
     if (isNaN(number)) throw new Error('One of the numbers is not a number');
   });
-  return numbers.reduce((acc, number) => acc + number, 0);
+  return numbers.reduce((acc, number) => acc + number);
 }
